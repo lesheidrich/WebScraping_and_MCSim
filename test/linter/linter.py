fc78f0afdb914819f75f249a52cf08dc863bd574
Les <77836486+lesheidrich@users.noreply.github.com>
2024-02-19 13:54:02 +0100

    lint and gitignore

--- test/linter/linter.py
+++ test/linter/linter.py
@@ -1,23 +1,66 @@
-# import os
-# from pylint import lint
-#
-#
-# def list_files_recursively(folder_path: str) -> [str]:
-#     file_list = []
-#     for root, dirs, files in os.walk(folder_path):
-#         dirs[:] = [d for d in dirs if "env" not in d]
-#         for file in files:
-#             if file.endswith(".py"):
-#                 file_list.append(os.path.join(root, file))
-#     return file_list
-#
-#
-# def run_linter(path: str, exceptions: list) -> None:
-#     files = list_files_recursively(path)
-#
-#     if not os.getcwd().endswith("linter"):
-#         os.chdir(os.path.join(path, "test", "linter"))
-#
-#     files[:] = [f for f in files if not any(module in f for module in exceptions)]
-#
-#     [lint.Run([f], exit=False) for f in files]
+"""
+Module: linter.py
+
+This module contains utility functions and classes for linting Python files in a project directory.
+It provides a TestLint class with methods to recursively list Python files and run the pylint linter
+on those files, excluding specified exceptions.
+
+Classes:
+    TestLint: A utility class for linting Python files in a project directory.
+
+Attributes:
+    PROJECT_FOLDER: A constant representing the root folder of the project.
+"""
+
+import os
+from typing import List
+from pylint import lint
+from project_secrets import PROJECT_FOLDER
+
+
+class TestLint:
+    """
+    A utility class for linting Python files in a project directory.
+
+    This class provides methods to recursively list Python files in a project directory and
+    run the pylint linter on those files, excluding specified exceptions.
+
+    Methods:
+        list_files_recursively: Recursively compiles a list of Python modules to lint from a project
+        directory.
+        run_linter: Lints all modules in the project directory, excluding specified exceptions.
+
+    Attributes:
+        PROJECT_FOLDER: The root folder of the project.
+    """
+    @staticmethod
+    def list_files_recursively(folder_path: str) -> List[str]:
+        """
+        Compiles list of modules to lint from project dir.
+        :param folder_path: str of project root dir
+        :return: str list of filepaths
+        """
+        file_list = []
+        for root, dirs, files in os.walk(folder_path):
+            dirs[:] = [d for d in dirs if "env" not in d]
+            for file in files:
+                if file.endswith(".py"):
+                    file_list.append(os.path.join(root, file))
+        return file_list
+
+    @staticmethod
+    def run_linter(exceptions: list, path: str = PROJECT_FOLDER) -> None:
+        """
+        Lints all modules in project directory, excluding param exceptions.
+        :param exceptions: list of str filenames to exclude from lint
+        :param path: path str to project dir
+        :return: None
+        """
+        files = TestLint.list_files_recursively(path)
+
+        if not os.getcwd().endswith("linter"):
+            os.chdir(os.path.join(path, "test", "linter"))
+        # filter out exceptions
+        files[:] = [f for f in files if not any(module in f for module in exceptions)]
+
+        [lint.Run([f], exit=False) for f in files]
--- test/regression_test.py
+++ test/regression_test.py
@@ -1,10 +1,10 @@
 import unittest
+
+from test.linter.linter import TestLint
 from test.unit.selenium_service_test import TestWebDriverFactory, TestChromeDriver, TestFirefoxDriver
 from test.unit.webscraper_utilities_test import TestWebKit, TestProxyKit
 from unit.proxy_handler_test import TestProxyHandler
 from unit.logger_test import TestLogger
-import os
-from pylint import lint
 
 
 def regression_test() -> unittest.TestSuite:
@@ -27,31 +27,8 @@ def regression_test() -> unittest.TestSuite:
     return test_suite
 
 
-def list_files_recursively(folder_path: str) -> [str]:
-    file_list = []
-    for root, dirs, files in os.walk(folder_path):
-        dirs[:] = [d for d in dirs if "env" not in d]
-        for file in files:
-            if file.endswith(".py"):
-                file_list.append(os.path.join(root, file))
-    return file_list
-
-
-def run_linter(path: str, exceptions: list) -> None:
-    files = list_files_recursively(path)
-
-    if not os.getcwd().endswith("linter"):
-        os.chdir(os.path.join(path, "test", "linter"))
-
-    files[:] = [f for f in files if not any(module in f for module in exceptions)]
-
-    [lint.Run([f], exit=False) for f in files]
-
-
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
     runner.run(regression_test())
 
-    path = 'C:\\Users\\dblin\\PycharmProjects\\WebScraping_and_MCSim'
-    exceptions = ["linter.py", "regression_test.py", "main.py"]
-    run_linter(path, exceptions)
+    TestLint.run_linter(exceptions=["regression_test.py", "main.py"])
