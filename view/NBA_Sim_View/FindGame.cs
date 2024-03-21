00c1241e8c7936808a1403fd9be1aedcbf66535a
Les <77836486+lesheidrich@users.noreply.github.com>
2024-03-21 23:35:16 +0100

    Add files via upload

---
+++ view/NBA_Sim_View/FindGame.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+
+namespace NBA_Sim_View
+{
+    public partial class FindGame : Form
+    {
+        private string dbHost = string.Empty;
+        private string season = string.Empty;         // e.g.: '1991-1992'
+        private string gameDate = string.Empty;
+        private string visitorTeam = string.Empty;
+        private string visitorPts = string.Empty;    
+        private string homeTeam = string.Empty;       // e.g.: 'Chicago Bulls'
+        private string homePts = string.Empty;
+        private string attendance = string.Empty;
+        private string arena = string.Empty;
+        private string gameType = string.Empty;
+        private string epochs = string.Empty;
+
+        public FindGame()
+        {
+            InitializeComponent();
+
+            InitializedgvGetGames();
+            InitialFormSettings();
+            rbDefault.Checked = true;
+            dgvGetGames.Visible = false;
+        }
+
+
+        // gets game info for dgv
+        private void btnGetGames_Click(object sender, EventArgs e)
+        {
+            // input check
+            if (cbHome.Text == "-- Home Team --" || cbAway.Text == "-- Away Team --" || cbSeason.Text == "-- Season --" ||
+                (rbCustom.Checked && tbDB.Text == ""))
+            {
+                string content = "Please complete selection process!";
+                MessageBox.Show(content, "SETUP ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (cbHome.Text == cbAway.Text)
+            {
+                string content = $"Teams cannot play themselves!\nHome = Away ({cbHome.Text})";
+                MessageBox.Show(content, "SETUP ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            epochs = tbEpochs.Text;
+            if (epochs == "" || epochs is null || !double.TryParse(epochs, out _))
+            {
+                string content = "Epoch number must be a numeric value!";
+                MessageBox.Show(content, "EPOCH ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // vals assgn to class attribs
+            dgvGetGames.Rows.Clear();
+            PopulateAttributes();
+
+            RestClient gamesClient = new RestClient("http://127.0.0.1:5000/monte_carlo/game_data");
+            RestRequest request = new RestRequest();
+            request.AddQueryParameter("home", homeTeam);
+            request.AddQueryParameter("away", visitorTeam);
+            request.AddQueryParameter("season", season);
+            request.AddQueryParameter("dbURI", dbHost);
+
+            try
+            {
+                RestResponse response = gamesClient.Post(request);
+
+                if (response.IsSuccessful)
+                {
+                    Response res = gamesClient.Deserialize<Response>(response).Data;
+
+                    try
+                    {
+                        List<GameData> gamesList = JsonSerializer.Deserialize<List<GameData>>(res.Message);
+
+                        if (gamesList.Count == 0)
+                        {
+                            MessageBox.Show("Teams did not play each other in season!", "DATA WARNING",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+
+                        dgvGetGames.Visible = true;
+                        btnSelect.Visible = true;
+                        btnForceScrape.Visible = true;
+                        foreach (var game in gamesList)
+                        {
+                            dgvGetGames.Rows.Add(
+                                game.id,
+                                game.date,
+                                game.visitor_team,
+                                game.visitor_pts,
+                                game.home_team,
+                                game.home_pts,
+                                game.attendance,
+                                game.arena,
+                                game.game_type
+                            );
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Failed to deserealize games list!\n{ex.Message}", "JSON ERROR",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Failed to make request! Status code: " + response.StatusCode, "REQUEST ERROR",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // save content from selected first row
+            SaveGameContentsFromRows(0);
+        }
+
+        private void btnInitialize_Click(object sender, EventArgs e)
+        {
+            // gets ok to proceed if in db, else str team name (e.g.: 'CHI') that is missing
+            RestClient cl = new RestClient("http://127.0.0.1:5000/monte_carlo/team_in_db");
+            RestRequest request = new RestRequest();
+
+            request.AddParameter("season", season);
+            request.AddParameter("home", homeTeam);
+            request.AddParameter("away", visitorTeam);
+            request.AddParameter("db", dbHost);
+
+            try
+            {
+                RestResponse response = cl.Get(request);
+
+                if (response.IsSuccessful)
+                {
+                    Response res = cl.Deserialize<Response>(response).Data;
+
+                    if (res.Status == 200 && res.Message == "")
+                    {
+                        MonteCarlo mc = new MonteCarlo(this, gameDate, visitorTeam, visitorPts, homeTeam, homePts, attendance,
+                         arena, gameType, epochs, season, dbHost);
+                        mc.Show();
+                        this.Hide();
+                    }
+                    else if (res.Status == 204 && res.Message.Length > 1)
+                    {
+                        ScrapePopUp sp = new ScrapePopUp(this, res.Message, gameDate, visitorTeam, visitorPts, homeTeam, homePts, attendance,
+                         arena, gameType, epochs, dbHost, season);
+                        sp.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Response returned error! Status code: " + res.Status, "RESPONSE ERROR",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Failed to make request! Status code: " + response.StatusCode, "REQUEST ERROR",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void rbDefault_CheckedChanged(object sender, EventArgs e)
+        {
+            tbDB.Enabled = false;
+            dbHost = string.Empty;
+            tbDB.Text = "URI connection str";
+        }
+
+        private void rbCustom_CheckedChanged(object sender, EventArgs e)
+        {
+            tbDB.Enabled = true;
+            tbDB.Text = "URI connection str";
+        }
+
+        private void tbDB_MouseClick(object sender, MouseEventArgs e)
+        {
+            tbDB.Text = string.Empty;
+        }
+
+        private void tbEpochs_MouseClick(object sender, MouseEventArgs e)
+        {
+            tbEpochs.Text = string.Empty;
+        }
+
+        private void btnSelectGame_Click(object sender, EventArgs e)
+        {
+            InitialFormSettings();
+        }
+
+        private void InitialFormSettings() 
+        {
+            rbDefault.Checked = true;
+            tbDB.Text = "URI connection str";
+            cbHome.Text = "-- Home Team --";
+            cbAway.Text = "-- Away Team --";
+            cbSeason.Text = "-- Season --";
+            dgvGetGames.Visible = false;
+            btnSelect.Visible = false;
+            btnForceScrape.Visible = false;
+        }
+
+        private void InitializedgvGetGames()
+        {
+            dgvGetGames.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dgvGetGames.Columns.Add("id", "ID");
+            dgvGetGames.Columns.Add("date", "Date");
+            dgvGetGames.Columns.Add("visitor_team", "Visitor Team");
+            dgvGetGames.Columns.Add("visitor_pts", "Visitor Points");
+            dgvGetGames.Columns.Add("home_team", "Home Team");
+            dgvGetGames.Columns.Add("home_pts", "Home Points");
+            dgvGetGames.Columns.Add("attendance", "Attendance");
+            dgvGetGames.Columns.Add("arena", "Arena");
+            dgvGetGames.Columns.Add("game_type", "Game Type");
+        }
+
+        private void dgvGetGames_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // gets content from cell click
+            SaveGameContentsFromRows(e.RowIndex);
+            //if (e.RowIndex >= 0 && e.RowIndex < dgvGetGames.Rows.Count)
+            //{
+            //    dgvGetGames.Rows[e.RowIndex].Selected = true;
+            //    DataGridViewRow row = dgvGetGames.Rows[e.RowIndex];
+
+            //    gameDate = row.Cells["date"].Value.ToString();
+            //    visitorTeam = row.Cells["visitor_team"].Value.ToString();
+            //    visitorPts = row.Cells["visitor_pts"].Value.ToString();
+            //    homeTeam = row.Cells["home_team"].Value.ToString();
+            //    homePts = row.Cells["home_pts"].Value.ToString();
+            //    attendance = row.Cells["attendance"].Value.ToString();
+            //    arena = row.Cells["arena"].Value.ToString();
+            //    gameType = row.Cells["game_type"].Value.ToString();
+            //}
+        }
+
+        private void SaveGameContentsFromRows(int index)
+        {
+            // gets content from cell click
+            if (index >= 0 && index < dgvGetGames.Rows.Count)
+            {
+                //dgvGetGames.Rows[index].Selected = true;
+                DataGridViewRow row = dgvGetGames.Rows[index];
+
+                gameDate = row.Cells["date"].Value.ToString();
+                visitorTeam = row.Cells["visitor_team"].Value.ToString();
+                visitorPts = row.Cells["visitor_pts"].Value.ToString();
+                homeTeam = row.Cells["home_team"].Value.ToString();
+                homePts = row.Cells["home_pts"].Value.ToString();
+                attendance = row.Cells["attendance"].Value.ToString();
+                arena = row.Cells["arena"].Value.ToString();
+                gameType = row.Cells["game_type"].Value.ToString();
+            }
+        }
+
+        private void PopulateAttributes()
+        {
+            homeTeam = cbHome.Text;
+            visitorTeam = cbAway.Text;
+            season = cbSeason.Text;
+            dbHost = tbDB.Text;
+
+            if (rbDefault.Checked)
+            {
+                dbHost = ProjectSecrets.DEFAULT_URI;
+            }
+        }
+
+        private void btnForceScrape_Click(object sender, EventArgs e)
+        {
+            ScrapePopUp sp = new ScrapePopUp(this, "FORCED", gameDate, visitorTeam, visitorPts, homeTeam, homePts, attendance,
+                arena, gameType, epochs, dbHost, season);
+            sp.Show();
+        }
+    }
+}
---
+++ view/NBA_Sim_View/ScrapePopUp.cs
@@ -0,0 +1,193 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace NBA_Sim_View
+{
+    public partial class ScrapePopUp : Form
+    {
+        private FindGame homeScreen;
+        private string proxyList = null;
+        private bool checkProxies = false;
+        private string missingStats = string.Empty;
+        private string gameDate = string.Empty;
+        private string visitorTeam = string.Empty;
+        private string visitorPts = string.Empty;
+        private string homeTeam = string.Empty;
+        private string homePts = string.Empty;
+        private string attendance = string.Empty;
+        private string arena = string.Empty;
+        private string gameType = string.Empty;
+        private string epochs = string.Empty;
+        private string dbHost = string.Empty;
+        private string season = string.Empty;
+        private bool forced = false;
+
+        private string scrapeMethod = "requests_scrape";
+        List<RadioButton> radioButtons;
+
+        public ScrapePopUp(FindGame window, string missingStats, string gameDate, string visitorTeam, string visitorPts, 
+            string homeTeam, string homePts, string attendance, string arena, string gameType, string epochs,
+            string dbHost, string season)
+        {
+            InitializeComponent();
+
+            homeScreen = window;
+            this.missingStats = missingStats;
+            radioButtons = new List<RadioButton> { rbRequests, rbFirefox, rbChrome, rbRequestsProxy, rbFirefoxProxy, 
+                rbChromeProxy };
+            this.gameDate = gameDate;
+            this.visitorTeam = visitorTeam;
+            this.visitorPts = visitorPts;
+            this.homeTeam = homeTeam;
+            this.homePts = homePts;
+            this.attendance = attendance;
+            this.arena = arena;
+            this.gameType = gameType;
+            this.epochs = epochs;
+            this.dbHost = dbHost;
+            this.season = season;
+
+            rbRequests.Checked = true;
+            WireUpEventHandlers();
+            tbMissing.Text = missingStats;
+        }
+
+        private void btnScrape_Click(object sender, EventArgs e)
+        {
+            if (checkProxies)
+            {
+                if (rbRequests.Checked || rbFirefox.Checked || rbChrome.Checked)
+                {
+                    lblCheckProxies_Click(sender, e);  // to false
+                }
+            }
+
+            btnScrape.Enabled = false;                 // further calls disabled until scrape finished
+            btnScrape.Text = "Scrape in Progress";
+            btnScrape.ForeColor = Color.Goldenrod;
+
+            if (missingStats == "FORCED")
+            {
+                forced = true;
+            }
+            Scrape();
+        }
+
+        public void Scrape()
+        {
+            var options = new RestClientOptions($"http://127.0.0.1:5000/monte_carlo/season_data");
+            options.MaxTimeout = 500000;
+
+            RestClient scrapeClient = new RestClient(options);
+
+            //RestClient scrapeClient = new RestClient("http://127.0.0.1:5000/monte_carlo/season_data");
+            RestRequest request = new RestRequest();
+
+            request.AddQueryParameter("check_proxies", checkProxies);
+            request.AddQueryParameter("proxy_list", proxyList);
+            request.AddQueryParameter("scrape_method", scrapeMethod);
+            request.AddQueryParameter("forced", forced);
+            request.AddParameter("season", season);
+            request.AddParameter("home", homeTeam);
+            request.AddParameter("away", visitorTeam);
+            request.AddParameter("db", dbHost);
+
+            try
+            {
+                RestResponse response = scrapeClient.Get(request);
+                if (response.IsSuccessful)
+                {
+                    Response res = scrapeClient.Deserialize<Response>(response).Data;
+
+                    if (res.Status == 200 && res.Message == "")
+                    {
+                        MonteCarlo mc = new MonteCarlo(homeScreen, gameDate, visitorTeam, visitorPts, homeTeam, homePts, attendance,
+                            arena, gameType, epochs, season, dbHost);
+                        mc.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Response returned error! Status code: " + res.Status, "RESPONSE ERROR",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Failed to make request! Status code: " + response.StatusCode, "REQUEST ERROR",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCustomProxyList_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog file = new OpenFileDialog())
+            {
+                file.Filter = "CSV Files (*.csv)|*.csv";
+                file.FilterIndex = 1;
+                file.RestoreDirectory = true;
+
+                DialogResult result = file.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    proxyList = file.FileName;
+                }
+            }
+            btnCustomProxyList.ForeColor = Color.Goldenrod;
+        }
+
+        private void lblCheckProxies_Click(object sender, EventArgs e)
+        {
+            if (checkProxies)
+            {
+                lblCheckProxies.ForeColor = Color.Snow;
+                checkProxies = false;
+            }
+            else
+            {
+                lblCheckProxies.ForeColor = Color.Goldenrod;
+                checkProxies = true;
+            }
+        }
+
+        // radio button change + name string change for scrape method in REST
+        private void WireUpEventHandlers()
+        {
+            foreach (RadioButton radioButton in radioButtons)
+            {
+                radioButton.CheckedChanged += UpdateScrapeMethod;
+            }
+        }
+
+        private void UpdateScrapeMethod(object sender, EventArgs e)
+        {
+            RadioButton selectedRadioButton = radioButtons.FirstOrDefault(rb => rb.Checked);
+            if (selectedRadioButton != null)
+            {
+                scrapeMethod = selectedRadioButton.Text.Replace(' ', '_').ToLower() + "_scrape";
+            }
+        }
+
+
+    }
+}
