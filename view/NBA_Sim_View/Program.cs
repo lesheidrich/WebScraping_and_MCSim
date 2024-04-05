ca76d0ac39146d615aac94917d0ac0514cfb1fcd
Les <77836486+lesheidrich@users.noreply.github.com>
2024-04-05 21:47:08 +0200

    Update Program.cs

--- view/NBA_Sim_View/Program.cs
+++ view/NBA_Sim_View/Program.cs
@@ -8,9 +8,6 @@ namespace NBA_Sim_View
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
